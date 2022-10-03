3b5fcd405fa186e912b807f64c96150c88021b27
Augusto Sigolo <augustosigolo@gmail.com>
2022-10-03 12:23:12 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.post('/log', async (req, res) => {
     console.log(await req.body);
     res.sendStatus(200);
